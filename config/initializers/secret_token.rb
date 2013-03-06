5c8d48734598d90866c468ce2d54e60312aff1e4
Chris Roos <chris@seagul.co.uk>
2013-03-06 17:00:19 +0000

    Empty rails 3.2.12 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsJsonCorsAndHttpAuth::Application.config.secret_token = '9858871492761764d94e2a611cac6cda7775bca890f7b4a2f12b80810bfad5d69c3fa168e25ceb165b70d9d2eb50834dc3c4fb276bbdb0d8c1350aee38cdd14b'
